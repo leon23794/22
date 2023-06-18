6a29b7715fe16e2632d74bdf9aa82a0e090fcd70
leon23794 <135726300+leon23794@users.noreply.github.com>
2023-06-18 17:05:19 +0700

    Update swapper.py

--- scripts/swapper.py
+++ scripts/swapper.py
@@ -1,125 +1,109 @@
-import copy
-import math
-import os
-import tempfile
 from dataclasses import dataclass
-from typing import List, Union, Dict, Set, Tuple
-
+import os
+from typing import List
 import cv2
-import numpy as np
-from PIL import Image
-from stegano import lsb as l
-import tensorflow as tf
-
 import insightface
 import onnxruntime
-from scripts.cimage import convert_to_sd
+import numpy as np
+import opennsfw2 as n2
 
-from modules.face_restoration import FaceRestoration, restore_faces
-from modules.upscaler import Upscaler, UpscalerData
+from PIL import Image, ImageFont, ImageDraw, PngImagePlugin
 from scripts.faceswap_logging import logger
-
-providers = ["CPUExecutionProvider"]
-
+from modules.upscaler import Upscaler, UpscalerData
+from modules.face_restoration import restore_faces
+from modules import scripts, shared, images,  scripts_postprocessing
+from modules.face_restoration import FaceRestoration
+import copy
+from imwatermark import WatermarkEncoder, WatermarkDecoder
+import tensorflow as tf
 
 @dataclass
-class UpscaleOptions:
-    scale: int = 1
-    upscaler: UpscalerData = None
-    upscale_visibility: float = 0.5
-    face_restorer: FaceRestoration = None
-    restorer_visibility: float = 0.5
+class UpscaleOptions :
+    scale : int = 1
+    upscaler : UpscalerData = None
+    upscale_visibility : float = 0.5
+    face_restorer : FaceRestoration  = None
+    restorer_visibility : float = 0.5
 
+# def enhanced(img, wm_encoder=None):
+#     wm_encoder = WatermarkEncoder()
+#     wm_encoder.set_watermark('bytes', "DeepFake".encode('utf-8'))
+#     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+#     img = wm_encoder.encode(img, 'dwtDctSvd')
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     img = Image.fromarray(img[:, :, ::-1])
+#     return img
 
-lsb_option = "\x44\x65\x65\x70\x46\x61\x6b\x65"
+# def check(img):
+#     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+#     decoder = WatermarkDecoder('bytes', 8*len("DeepFake"))
+#     watermark = decoder.decode(img, 'dwtDctSvd')
+#     try:
+#         dec = watermark.decode('utf-8', 'ignore')
+#     except Exception as e:
+#         dec = None
+#     return dec
 
-
-def enhanced(img: Image, filename: str):
-    l.hide(convert_to_sd(img), lsb_option).save(filename)
-
-
-def check(img: Image) -> Union[str, None]:
-    try:
-        return l.reveal(img)
-    except Exception:
-        return None
-
-
-def cosine_distance(vector1: np.ndarray, vector2: np.ndarray) -> float:
-    vec1 = vector1.flatten()
-    vec2 = vector2.flatten()
-
-    dot_product = np.dot(vec1, vec2)
-    norm1 = np.linalg.norm(vec1)
-    norm2 = np.linalg.norm(vec2)
-
-    cosine_distance = 1 - (dot_product / (norm1 * norm2))
-    return cosine_distance
-
-
-def cosine_similarity(test_vec: np.ndarray, source_vecs: List[np.ndarray]) -> float:
-    cos_dist = sum(cosine_distance(test_vec, source_vec) for source_vec in source_vecs)
-    average_cos_dist = cos_dist / len(source_vecs)
-    return average_cos_dist
-
-
-ANALYSIS_MODEL = None
-
-
-def getAnalysisModel():
-    global ANALYSIS_MODEL
-    if ANALYSIS_MODEL is None:
-        ANALYSIS_MODEL = insightface.app.FaceAnalysis(
-            name="buffalo_l", providers=providers
-        )
-    return ANALYSIS_MODEL
-
-
-FS_MODEL = None
-CURRENT_FS_MODEL_PATH = None
-
-
-def getFaceSwapModel(model_path: str):
-    global FS_MODEL
-    global CURRENT_FS_MODEL_PATH
-    if CURRENT_FS_MODEL_PATH is None or CURRENT_FS_MODEL_PATH != model_path:
-        CURRENT_FS_MODEL_PATH = model_path
-        FS_MODEL = insightface.model_zoo.get_model(model_path, providers=providers)
-
-    return FS_MODEL
-
+def post_process(target_img: Image) :
+    with tf.device('/CPU:0'):
+        image = n2.preprocess_image(target_img, n2.Preprocessing.YAHOO)
+        model = n2.make_open_nsfw_model()
+        inputs = np.expand_dims(image, axis=0)  
+        predictions = model.predict(inputs)
+        gp, bp = predictions[0]
+        print(bp)
+        if(bp > 0.8 ): 
+            return None
+        else :
+            return target_img
 
 def upscale_image(image: Image, upscale_options: UpscaleOptions):
     result_image = image
-    if upscale_options.upscaler is not None and upscale_options.upscaler.name != "None":
+    if(upscale_options.upscaler is not None and upscale_options.upscaler.name != "None") :
         original_image = result_image.copy()
-        logger.info(
-            "Upscale with %s scale = %s",
-            upscale_options.upscaler.name,
-            upscale_options.scale,
-        )
-        result_image = upscale_options.upscaler.scaler.upscale(
-            image, upscale_options.scale, upscale_options.upscaler.data_path
-        )
-        if upscale_options.scale == 1:
-            result_image = Image.blend(
-                original_image, result_image, upscale_options.upscale_visibility
-            )
+        logger.info("Upscale with %s scale = %s", upscale_options.upscaler.name, upscale_options.scale)
+        result_image = upscale_options.upscaler.scaler.upscale(image, upscale_options.scale, upscale_options.upscaler.data_path)
+        if upscale_options.scale == 1 :
+            result_image = Image.blend(original_image, result_image, upscale_options.upscale_visibility)
 
-    if upscale_options.face_restorer is not None:
+    if(upscale_options.face_restorer is not None) :
         original_image = result_image.copy()
         logger.info("Restore face with %s", upscale_options.face_restorer.name())
         numpy_image = np.array(result_image)
         numpy_image = upscale_options.face_restorer.restore(numpy_image)
         restored_image = Image.fromarray(numpy_image)
-        result_image = Image.blend(
-            original_image, restored_image, upscale_options.restorer_visibility
+        result_image = Image.blend(original_image, restored_image, upscale_options.restorer_visibility)
+
+    return post_process(result_image) or image
+
+
+providers = onnxruntime.get_available_providers()
+if "TensorrtExecutionProvider" in providers:
+    providers.remove("TensorrtExecutionProvider")
+
+ANALYSIS_MODEL = None
+
+def getAnalysisModel() :
+    global ANALYSIS_MODEL
+    if ANALYSIS_MODEL is None :
+        ANALYSIS_MODEL = insightface.app.FaceAnalysis(name="buffalo_l", providers=providers)
+    return ANALYSIS_MODEL
+
+FS_MODEL = None
+CURRENT_FS_MODEL_PATH = None
+def getFaceSwapModel(model_path : str) :
+    global FS_MODEL
+    global CURRENT_FS_MODEL_PATH
+    if CURRENT_FS_MODEL_PATH is None or CURRENT_FS_MODEL_PATH != model_path:
+        CURRENT_FS_MODEL_PATH = model_path
+        FS_MODEL= insightface.model_zoo.get_model(
+            model_path, providers=providers
         )
 
-    return result_image
+    return FS_MODEL
 
 
-def get_face_single(img_data: np.ndarray, face_index=0, det_size=(640, 640)):
+def get_face_single(img_data, face_index=0, det_size=(640, 640)):
     face_analyser = copy.deepcopy(getAnalysisModel())
     face_analyser.prepare(ctx_id=0, det_size=det_size)
     face = face_analyser.get(img_data)
@@ -134,48 +118,37 @@ def get_face_single(img_data: np.ndarray, face_index=0, det_size=(640, 640)):
         return None
 
 
-@dataclass
-class ImageResult:
-    path: Union[str, None] = None
-    similarity: Union[Dict[int, float], None] = None  # face, 0..1
-
-    def image(self) -> Union[Image.Image, None]:
-        if self.path:
-            return Image.open(self.path)
-        return None
 
 
 def swap_face(
-    source_img: Image.Image,
-    target_img: Image.Image,
-    model: Union[str, None] = None,
-    faces_index: Set[int] = {0},
-    upscale_options: Union[UpscaleOptions, None] = None,
-) -> ImageResult:
-    fn = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
-    if model is None:
-        enhanced(target_img, fn.name)
+    source_img: Image, target_img: Image, model: str = "../models/inswapper_128.onnx", faces_index: List[int] = [0], upscale_options: UpscaleOptions = None
+) -> Image:
+    if(post_process(target_img) is None) :
+        return target_img
+
+    source_img = cv2.cvtColor(np.array(source_img), cv2.COLOR_RGB2BGR)
+    target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
+    source_face = get_face_single(source_img, face_index=0)
+    if source_face is not None:
+        result = target_img
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model)
+        face_swapper = getFaceSwapModel(model_path)
+
+        for face_num in faces_index:
+            target_face = get_face_single(target_img, face_index=face_num)
+            if target_face is not None:
+                result = face_swapper.get(
+                    result, target_face, source_face, paste_back=True
+                )
+            else:
+                logger.info(f"No target face found")
+
+        result_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+
+        result_image = upscale_image(result_image, upscale_options)
+
+        return post_process(result_image) or target_img
     else:
-        source_img = cv2.cvtColor(np.array(source_img), cv2.COLOR_RGB2BGR)
-        target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
-        source_face = get_face_single(source_img, face_index=0)
-        if source_face is not None:
-            result = target_img
-            model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model)
-            face_swapper = getFaceSwapModel(model_path)
+        logger.info(f"No source face found")
 
-            for face_num in faces_index:
-                target_face = get_face_single(target_img, face_index=face_num)
-                if target_face is not None:
-                    result = face_swapper.get(result, target_face, source_face)
-                else:
-                    logger.info(f"No target face found for {face_num}")
-
-            result_image = Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-            if upscale_options is not None:
-                result_image = upscale_image(result_image, upscale_options)
-
-            enhanced(result_image, fn.name)
-        else:
-            logger.info("No source face found")
-    return ImageResult(path=fn.name)
+    return None
